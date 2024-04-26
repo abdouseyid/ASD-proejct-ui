9da444a544fc60b1bec1137684632f968332d0c0
kdraa12 <med3744lemin@gmail.com>
2024-04-26 09:43:44 -0500

    chang vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import type { StorybookConfig } from '@storybook/react-vite'
+import { withoutVitePlugins } from '@storybook/builder-vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['uuid']
-    }
-  }
-})
+const config: StorybookConfig = {
+	stories: ['../ts/**/*.stories.mdx', '../ts/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-storysource',
+		'storybook-dark-mode',
+		{
+			name: '@storybook/addon-styling',
+			options: {
+				postCss: true
+			}
+		}
+	],
+	framework: {
+		name: '@storybook/react-vite',
+		options: {}
+	},
+// Remove the 'storyStoreV7' property
+// features: {
+//     storyStoreV7: true
+// },
+	typescript: {
+		check: false
+	},
+	docs: {
+		autodocs: true
+	},
+viteFinal: async config => {
+	return { ...config, plugins: await withoutVitePlugins(config.plugins, ['node-externals']) }
+	}
+}
 
+export default config
